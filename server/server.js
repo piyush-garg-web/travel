1157af8c191279bfedebb89d3665ef9faa6bdf1a
piyushgarg6702-cyber <piyushgarg6702@gmail.com>
2026-08-23 22:05:15 +0530

    done

--- server/server.js
+++ server/server.js
@@ -20,6 +20,14 @@ app.use(cors({
 app.use(express.json());
 
 // Routes
+// Routes
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'JanYatri backend is running!'
+  });
+});
+
 app.use('/api/auth', authRoutes);
 
 // Error handling middleware
@@ -29,6 +37,11 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
